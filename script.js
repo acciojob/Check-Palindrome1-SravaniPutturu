4fed2fabad2de89264bd3f4547c2bb7660ceedd1
SravaniPutturu <117716430+SravaniPutturu@users.noreply.github.com>
2023-04-07 09:38:11 +0530

    created a submission

--- script.js
+++ script.js
@@ -3,6 +3,6 @@
 function palindrome(str){
 	
 	str = str.replace(" ","").toLowerCase()
-	return str == str.split("")reverse().join("");
+	return str == str.split("").reverse().join("");
 }
 module.exports = palindrome
